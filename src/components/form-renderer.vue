357e74e8d643a5a7d765536bca86b0bce1d61cdb
Salomon Dion <dev.mrdion@gmail.com>
2019-06-09 06:28:14 +0000

    add form renderer

---
+++ src/components/form-renderer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="formeo"></div>
+</template>
+
+<script>
+import { FormeoRenderer } from 'formeo'
+import 'formeo/dist/formeo.min.css'
+
+export default {
+  name: 'FormRenderer',
+  props: {
+    data: {
+      type: Object,
+      default: () => ({}),
+    },
+    formeoOpts: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    formeo: null,
+  }),
+  mounted() {
+    this.formeoOpts.renderContainer = this.$refs.formeo
+    this.formeoOpts.external = {}
+    this.formeo = new FormeoRenderer(this.formeoOpts)
+    this.formeo.render(this.data)
+  },
+  methods: {},
+}
+</script>
