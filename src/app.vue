f5e8fbe722733f5ad43c1f926d8fea3f42e5851c
Salomon Dion <dev.mrdion@gmail.com>
2019-06-11 21:11:21 +0000

    add application wide events

--- src/app.vue
+++ src/app.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="app-root">
-    <router-view></router-view>
+    <router-view :key="$route.name"></router-view>
     <!-- global snackbar -->
     <v-snackbar
-      :timeout="3000"
-      bottom
-      right
-      :color="snackbar.color"
       v-model="snackbar.show"
+      :color="snackbar.color"
+      :bottom="snackbar.y === 'bottom'"
+      :left="snackbar.x === 'left'"
+      :multi-line="snackbar.mode === 'multi-line'"
+      :right="snackbar.x === 'right'"
+      :timeout="snackbar.timeout"
+      :top="snackbar.y === 'top'"
+      :vertical="snackbar.mode === 'vertical'"
     >
       {{ snackbar.text }}
-      <v-btn dark flat @click.native="snackbar.show = false" icon>
+      <v-btn dark flat icon @click.native="snackbar.show = false">
         <v-icon>close</v-icon>
       </v-btn>
     </v-snackbar>
@@ -19,6 +23,7 @@
 
 <script>
 import NetworkStatusIndicator from '@/components/network-status-indicator.vue'
+import AppEvents from '@/event'
 
 export default {
   components: {
@@ -31,8 +36,18 @@ export default {
         show: false,
         text: '',
         color: '',
+        y: 'top',
+        x: null,
+        timeout: 6000,
+        mode: '',
       },
     }
+  },
+  created() {
+    AppEvents.forEach((event) => {
+      this.$on(event.name, event.callback)
+    })
+    window.$app = this
   },
 }
 </script>
--- src/event.js
+++ src/event.js
@@ -1,61 +1,17 @@
 export default [
   {
-    name: 'APP_LOGIN_SUCCESS',
-    callback: function (e) {
-      this.$router.push({ path: 'dashboard' });
-    }
+    name: 'APP_SYNCED',
+    callback: function(e) {},
   },
   {
-    name: 'APP_LOGOUT',
-    callback: function (e) {
+    name: 'APP_RESOURCE_SAVED',
+    callback: function(e) {
       this.snackbar = {
+        ...this.snackbar,
         show: true,
         color: 'green',
-        text: 'Logout successfully.'
-      };
-      this.$router.replace({ path: '/login' });
-    }
+        text: 'This item was saved successfully.',
+      }
+    },
   },
-  {
-    name: 'APP_PAGE_LOADED',
-    callback: function (e) {
-    }
-  },
-  {
-    name: 'APP_AUTH_FAILED',
-    callback: function (e) {
-      this.$router.push('/login');
-      this.$message.error('Token has expired');
-    }
-  },
-  {
-    name: 'APP_BAD_REQUEST',
-    // @error api response data
-    callback: function (msg) {
-      this.$message.error(msg);
-    }
-  },
-  {
-    name: 'APP_ACCESS_DENIED',
-    // @error api response data
-    callback: function (msg) {
-      this.$message.error(msg);
-      this.$router.push('/forbidden');
-    }
-  },
-  {
-    name: 'APP_RESOURCE_DELETED',
-    // @error api response data
-    callback: function (msg) {
-      this.$message.success(msg);
-    }
-  },
-  {
-    name: 'APP_RESOURCE_UPDATED',
-    // @error api response data
-    callback: function (msg) {
-      this.$message.success(msg);
-    }
-  },
-
-];
+]
