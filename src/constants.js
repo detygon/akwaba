3c7ae89bb4e53f0b4e0a2fa812d692abd5b5780c
Salomon Dion <dev.mrdion@gmail.com>
2019-06-19 12:41:42 +0000

    add a default value for api backend

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const dbName = process.env.VUE_APP_DB_NAME || 'akwaba'
 export const syncURL = process.env.VUE_APP_SYNC_URL || 'http://localhost:5984'
 export const dbRemote = `${syncURL}/${dbName}`
-export const API_BASE_URL = process.env.VUE_APP_API
+export const API_BASE_URL = process.env.VUE_APP_API || 'http://localhost:1337'
